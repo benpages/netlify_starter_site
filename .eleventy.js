6b8b4e871dff64b9c80280287d950001518523c8
benpages <86982605+benpages@users.noreply.github.com>
2022-10-27 14:53:47 +0200

    Added redirect file

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,22 @@
-const { DateTime } = require("luxon");
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("./src/css");
-	eleventyConfig.addPassthroughCopy("./src/js");
-	eleventyConfig.addPassthroughCopy("./src/assets");
-	eleventyConfig.addPassthroughCopy("./src/admin");
+	eleventyConfig.addPassthroughCopy('./src/css');
+	eleventyConfig.addPassthroughCopy('./src/js');
+	eleventyConfig.addPassthroughCopy('./src/assets');
+	eleventyConfig.addPassthroughCopy('./src/admin');
+	eleventyConfig.addPassthroughCopy('src/_redirects');
 
-	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
-	eleventyConfig.addFilter("postDate", (dateObj) => {
+	eleventyConfig.addFilter('postDate', (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
 	});
 
 	return {
 		dir: {
-			input: "src",
-			output: "public",
+			input: 'src',
+			output: 'public',
 		},
 	};
 };
